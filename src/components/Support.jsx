aca78b5f23b6a68ee8646f27e53a95e6cefdc48d
LisaxLF <112825800+LisaxLF@users.noreply.github.com>
2024-08-18 12:23:33 +0000

    finished 1_3b

---
+++ src/components/Support.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export default () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <button className="outline" onClick={() => setCount(count + 1)} >
+    
+    {/* {(() => {
+      if (count === 0) {
+        return 'Click to support'
+      } else {
+        return `Supported ${count} times`
+      }
+    })()} */}
+
+    {count === 0 ? "Click to support" : `You've supported ${count} times`}
+    
+    </button>
+  )
+}
