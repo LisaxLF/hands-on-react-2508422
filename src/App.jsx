a4133682211532149673b024551e092d1aa0aa1e
LisaxLF <112825800+LisaxLF@users.noreply.github.com>
2024-08-18 11:36:46 +0000

    finish assignment 1_1b

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 function App() {
+  const name = 'StarGazers'
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="Stargazer" />
+          <h1>Meet the <b><i style={{color: "steelblue", fontSize: "2.5rem"}}>{name}</i></b></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+            <button className="outline" onClick={() => alert('Hi there')}>
+              click me
+            </button>
+        </hgroup>
+      </article>
+    </div>
   )
+  
 }
 export default App
