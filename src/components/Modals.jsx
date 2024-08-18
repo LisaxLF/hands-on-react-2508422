b27441074207117f1d6c07019428eb2d485980dc
LisaxLF <112825800+LisaxLF@users.noreply.github.com>
2024-08-18 15:26:06 +0000

    finished 2_5b

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,28 +1,62 @@
-export default ({ member, handleClose }) => {
+export default ({ member, handleClose, handleChange }) => {
   return (
     <dialog id="modal-member" open>
       <article>
-        <a href="#close"
+        <a
+          href="#close"
           aria-label="Close"
           className="close"
           data-target="modal-member"
           onClick={handleClose}
         ></a>
         <hgroup>
-          <div style={{
-            display: "flex",
-            gap: '1rem'
-          }}>
-            <img style={{ width: '200px' }}
+          <div
+            style={{
+              display: "flex",
+              gap: "1rem",
+            }}
+          >
+            <img
+              style={{ width: "200px" }}
               src={`images/${member.slug}.svg`}
-              alt={member.name} />
+              alt={member.name}
+            />
             <hgroup>
-              <h1>{member.name}</h1>
+              <h1>
+                {member.name} - {member.id}
+              </h1>
               <p>{member.bio}</p>
             </hgroup>
           </div>
         </hgroup>
+        <hgroup
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <a
+            className="outline"
+            href="#"
+            role="button"
+            onClick={() => {
+              handleChange(Number(member.id - 1));
+            }}
+          >
+            prev
+          </a>
+          <a
+            className="outline"
+            href="#"
+            role="button"
+            onClick={() => {
+              handleChange(Number(member.id + 1));
+            }}
+          >
+            next
+          </a>
+        </hgroup>
       </article>
     </dialog>
-  )
-}
+  );
+};
